2ede78563bd2302243f7f9b32e95095d69bc455e
tataata <tataata@tataata-TATAATA.local>
2013-03-05 20:55:23 +0100

    move js from html to separate file

--- js/script.js
+++ js/script.js
@@ -5,6 +5,20 @@ Web: http://taniaabanina.com
 
 */
 
+$(document).ready(function() {
+
+  $(".input")
+  .focus(function () {
+    $(this).prev("label").removeClass("show").addClass("hide");
+  })
+  .blur(function() {
+    if ($(this).val() === '') {
+      $(this).prev("label").addClass("show").removeClass("hide");
+    }
+  });
+
+});
+
 
 
 
